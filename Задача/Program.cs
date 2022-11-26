9800f5b358d2057f07d4c05be9d4a9a82a4cf82a
shpakovaei <e.i.shpakova@mail.ru>
2022-11-26 14:23:52 +0600

    задание

---
+++ Задача/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. 
+
+string[] array1 = new string[7] { "Animals", "dog", "cat", "mouse", "pig", "sheep", "horse" };
+string[] arrayNew = new string[array1.Length];
+
+void FillArray(string[] array1, string[] arrayNew)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            arrayNew[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FillArray(array1, arrayNew);
+PrintArray(arrayNew);
